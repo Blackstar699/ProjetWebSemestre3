7b009def95c08ebc6c982ace792fd4dc39265a91
Zekir <gianlucaltz@gmail.com>
2021-01-12 12:05:14 +0100

    premiere modifs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        isLargeRow
+        title="Action"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        isLargeRow
+        title="Fantastique"
+        fetchUrl={requests.fetchFantasyMovies}
+      />
+      <Row
+        isLargeRow
+        title="Science-fiction"
+        fetchUrl={requests.fetchScienceFictionMovies}
+      />
+      <Row
+        isLargeRow
+        title="Horreur"
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row
+        isLargeRow
+        title="Thriller"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
     </div>
   );
 }
